0bbd82d70f7eed6bef7390f5980bf4e503b77a06
unknown <19pa1a05i5@vishnu.edu.in>
2022-04-02 15:02:41 +0530

    my first version

---
+++ src/components/MainNav.js
@@ -0,0 +1,82 @@
+// import  React from 'react';
+// import BottomNavigation from "@mui/material/BottomNavigation";
+// import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+// import RestoreIcon from '@mui/icons-material/Restore';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import LocationOnIcon from '@mui/icons-material/LocationOn';
+// import { makeStyles } from '@material-ui/core';
+
+
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+// import RestoreIcon from "@material-ui/icons/Restore";
+// import FavoriteIcon from "@material-ui/icons/Favorite";
+// import LocationOnIcon from "@material-ui/icons/LocationOn";
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import TvIcon from "@material-ui/icons/Tv";
+import MovieIcon from "@material-ui/icons/Movie";
+import SearchIcon from "@material-ui/icons/Search";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+const useStyles = makeStyles({
+    root: {
+     width: "100%",
+    position: "fixed",
+    bottom: 0,
+    backgroundColor: "#2d313a",
+    zIndex: 100,
+    }
+});
+export default function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const [value, setValue] = React.useState(0);
+  useEffect(() => {
+    if (value === 0) {history.push("/");
+        } else if (value === 1) {
+      history.push("/movies");
+    } else if (value === 2) {
+      history.push("/series");
+    } else if (value === 3) {
+      history.push("/search");
+    }
+  
+  }, [value,history]);
+
+
+
+    return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+            <BottomNavigationAction
+                   style={{ color: "white" }}
+                label="Trending" icon={<WhatshotIcon />} />
+       <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Movies"
+        icon={<MovieIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="TV Series"
+        icon={<TvIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Search"
+        icon={<SearchIcon />}
+      />
+
+      </BottomNavigation>
+
+  );
+}
